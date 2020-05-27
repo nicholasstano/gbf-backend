05fc3d08d123fe8fcf9ee77342613bfecd406651
nicholasstano <nicholasstano@gmail.com>
2020-05-27 10:30:22 -0400

    Setup User database

---
+++ db/migrate/20200527141540_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :teacher_name
+      t.boolean :is_teacher
+      t.boolean :is_coach
+      t.boolean :is_leader
+      t.string :organization
+
+      t.timestamps
+    end
+  end
+end
