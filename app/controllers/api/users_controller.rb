a7f3a3b65d1bf092f5e639b60c4a80537559d3ad
nicholasstano <nicholasstano@gmail.com>
2020-05-27 10:47:10 -0400

    Create new user working

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,4 +3,18 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: users
     end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          render json: user
+        else
+          render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:id, :username, :password, :teacher_name, :is_teacher, :is_coach, :is_leader, :organization)
+    end
 end
