6706e6a6c70c5d78043c6463ff67db4787ab7c1c
nicholasstano <nicholasstano@gmail.com>
2020-06-17 13:28:51 -0400

    Set up Belongs to and Has Many on most tables.

--- app/models/observation.rb
+++ app/models/observation.rb
@@ -1,4 +1,4 @@
 class Observation < ApplicationRecord
-    belongs_to :teacher, class_name: 'User', required: false
+    belongs_to :teacher, class_name: 'User', foreign_key: 'teacherId', required: false
     belongs_to :coach, class_name: 'User', required: false
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    has_many :teacher_coaches
-    has_many :observations
+    has_many :teacher_coaches, foreign_key: "coachId"
+    has_many :observations, foreign_key: 'teacherId'
     has_many :feedbacks, foreign_key: 'teacherId'
-    has_many :action_steps
+    has_many :action_steps, foreign_key: 'teacherId'
 end
