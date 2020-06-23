86f3af5a5c2d6c11aeea0297fd7edc2a175147f1
nicholasstano <nicholasstano@gmail.com>
2020-06-23 15:48:31 -0400

    added a delete to observations and got the frontend and backend set up so you can add a new observation using the form on the frontend.

--- Gemfile
+++ Gemfile
@@ -26,7 +26,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
-
+# Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
+gem 'rack-cors', :require => 'rack/cors'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
--- app/controllers/api/v1/observations_controller.rb
+++ app/controllers/api/v1/observations_controller.rb
@@ -9,6 +9,11 @@ class Api::V1::ObservationsController < ApplicationController
         render json: observation
     end
 
+    def destroy
+        observation = Observation.find(params[:id])
+        observation.delete
+    end
+
     private 
     def observation_params
         params.require(:observation).permit(:id, :teacherId, :coachId, :date, :move, :score, :notes, :comments, :is_released)
