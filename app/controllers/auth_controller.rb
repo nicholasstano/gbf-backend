1b8cb7689eb05949a26e0b2462a6ed9230773f8c
nicholasstano <nicholasstano@gmail.com>
2020-07-03 14:34:33 -0400

    Added some Login Logic

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,12 @@
+class AuthController < ApplicationController
+    protect_from_forgery
+
+    def login
+        user = User.find_by(username: params[:username])
+        if (user && user.authenticate(params[:password]))
+          render json: user
+        else
+          render json: {errors: "Invalid email/password combination. Please try again or register if you do not already have an account."}
+        end
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/login', to: 'auth#login'
+
   namespace :api do
     namespace :v1 do
       resources :users
